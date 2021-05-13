2012e6798cb6511edf2e7e4e7fd89a832638d6c3
Damián Alvarez <alvarezdami93@gmail.com>
2021-05-13 08:15:03 -0300

    Add progress bar view logic and sound when time is up

--- EggTimer/AppDelegate.swift
+++ EggTimer/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
-//  AppDelegate.swift
+//  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Damián Alvarez on 05/10/2021.
+//  Copyright © 2021 Radium Rocket. All rights reserved.
 //
 
 import UIKit
--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,42 +2,78 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Damián Alvarez on 05/10/2021.
+//  Copyright © 2021 Radium Rocket. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
     let eggTime = [
         "Soft": 3, // 300,
         "Medium": 5,// 420,
         "Hard": 7//720
     ]
+    
     var timer = Timer()
-    var secondsRemaining = 60
+    var total = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer?
 
     @IBOutlet weak var countDownLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print("Instantiate \(sender.currentTitle!)")
-        let hardness = sender.currentTitle!
 
-        secondsRemaining = eggTime[hardness]!
+        let hardness = sender.currentTitle!
+        countDownLabel.text = hardness
+        secondsPassed = 0
+        progressBar.progress = 0.00
+        total = eggTime[hardness]!
+
         // just in case this button is tapped multiple times
         timer.invalidate()
+
         // start the timer
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-    }
-    
-    @objc func updateTimer() {
-        secondsRemaining -= 1
-        countDownLabel.text = String(secondsRemaining)
 
-        if secondsRemaining == 0 {
+    }
+
+    @objc func updateTimer() {
+
+        secondsPassed += 1
+        progressBar.progress = Float(secondsPassed) / Float(total)
+        
+        if secondsPassed == total {
             timer.invalidate()
             countDownLabel.text = "It's time!"
+            progressBar.progress = 1
+            playSound()
         }
+
     }
     
+    func playSound() {
+
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print (error.localizedDescription)
+        }
+
+    }
+
 }
